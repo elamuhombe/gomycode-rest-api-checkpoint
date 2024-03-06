130c1e803c3e622ac07ab5c982fe69bf90019ec2
Elaine Muhombe <emuhombe@gmail.com>
2024-03-06 21:36:36 +0300

    chore: update server.js - import body-parser

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const User= require('./models/User');
 require('dotenv').config();
 
